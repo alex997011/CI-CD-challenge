ff46b420d527f7262e556a7b0bf81a8ad2405241
Alejandro <alejandropacheog45@gmail.com>
2024-11-11 20:43:51 -0500

    LoginPage Configuration

--- DemoAutomationProject/src/main/java/com/globant/app/pages/LoginPage.java
+++ DemoAutomationProject/src/main/java/com/globant/app/pages/LoginPage.java
@@ -1,4 +1,36 @@
 package com.globant.app.pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginPage extends BasePage {
+    private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
+
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void login(String username, String password) {
+        log.info("Waiting to Login");
+        wait.until(ExpectedConditions.visibilityOf(usernameInput)).sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginButton.click();
+        log.info("Successful Login");
+
+    }
+
 }
