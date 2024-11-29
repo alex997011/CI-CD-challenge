e7cfbb081238d75a9e75de65974ddeb5249e45c6
Alejandro Pacheco <alejandropachecog45@gmail.com>
2024-11-29 14:59:35 -0500

    Update LogoutTest.java

--- DemoAutomationProject/src/test/java/com/globant/app/tests/LogoutTest.java
+++ DemoAutomationProject/src/test/java/com/globant/app/tests/LogoutTest.java
@@ -4,10 +4,17 @@ import com.globant.app.pages.LoginPage;
 import com.globant.app.pages.ProductPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 
 public class LogoutTest extends BaseTest{
 
     @Test
+    @Description("Logout Test Description")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Logout Functionality")
     public void completeLogoutFlow (){
 
         LoginPage loginPage = new LoginPage(driver);
