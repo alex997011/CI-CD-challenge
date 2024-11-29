c1b4e18906f9b49ee4e3ceb20bea8511895f33c8
alex997011 <alejandropachecog45@gmail.com>
2024-11-29 13:56:52 -0500

    Added headless mode Config for CI/CD

--- DemoAutomationProject/src/test/java/com/globant/app/tests/BaseTest.java
+++ DemoAutomationProject/src/test/java/com/globant/app/tests/BaseTest.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.log4testng.Logger;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class BaseTest {
     protected WebDriver driver;
@@ -13,8 +15,14 @@ public class BaseTest {
 
     @BeforeMethod
     public void setUp() {
-
         WebDriverManager.chromedriver().setup();
+
+        // Configurar opciones de Chrome para modo headless
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Para GitHub Actions
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.saucedemo.com/");
