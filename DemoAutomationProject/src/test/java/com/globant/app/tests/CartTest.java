0949c08c948e768d23d6d43258aa4bfc247cbffa
Alejandro Pacheco <alejandropachecog45@gmail.com>
2024-11-29 15:00:24 -0500

    Update CartTest.java

--- DemoAutomationProject/src/test/java/com/globant/app/tests/CartTest.java
+++ DemoAutomationProject/src/test/java/com/globant/app/tests/CartTest.java
@@ -4,11 +4,20 @@ import com.globant.app.pages.CartPage;
 import com.globant.app.pages.LoginPage;
 import com.globant.app.pages.ProductPage;
 import org.testng.annotations.Test;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
 
 import java.util.List;
 
 public class CartTest extends BaseTest {
-    @Test public void completeRemoveFlow(){
+    @Test 
+    @Description("Cart Test Description")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Cart Functionality")
+    
+    public void completeRemoveFlow(){
         LoginPage loginPage = new LoginPage(driver);
         ProductPage productPage = new ProductPage(driver);
         CartPage cartPage = new CartPage(driver);
